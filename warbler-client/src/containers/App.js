3eeade315751503ddef182ff1c1f1bcbe47fd743
gksapori <gerrysaporito@gmail.com>
2020-04-21 00:39:53 -0400

    Set up redux and created navbar

---
+++ warbler-client/src/containers/App.js
@@ -0,0 +1,21 @@
+import React from 'react';
+import { Provider } from 'react-redux';
+import { BrowserRouter as Router } from 'react-router-dom';
+import{ configureStore } from '../store';
+
+import Navbar from './Navbar';
+
+const store = configureStore();
+
+const App = () => (
+  <Provider store={store}>
+    <Router>
+      <div className='onboarding'>
+        <Navbar />
+
+      </div>
+    </Router>
+  </Provider>
+);
+
+export default App;
