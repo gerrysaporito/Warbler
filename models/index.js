90b2bbea4915e6d1b8537e2b0ff487fa75aedc6c
Gerry Saporito <gerrysaporito@gmail.com>
2020-07-07 12:23:10 -0400

    Uses .env file for db

--- models/index.js
+++ models/index.js
@@ -1,11 +1,12 @@
-const mongoose = require('mongoose');
-mongoose.set('debug', true);
+require("dotenv").config();
+const mongoose = require("mongoose");
+mongoose.set("debug", true);
 mongoose.Promise = Promise;
-mongoose.connect('mongodb://localhost/warbler', {
-    keepAlive: true,
-    useNewUrlParser: true,
-    useUnifiedTopology: true,
+mongoose.connect(process.env.MONGODB_URI, {
+  keepAlive: true,
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
 });
 
-module.exports.User = require('./user');
-module.exports.Message = require('./message');
+module.exports.User = require("./user");
+module.exports.Message = require("./message");
